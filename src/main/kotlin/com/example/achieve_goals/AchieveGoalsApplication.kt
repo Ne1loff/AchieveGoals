88d1c3eb918b29df2f68b172c99449b560b47e71
xinik <sergey20160@gmail.com>
2021-09-04 10:52:34 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/achieve_goals/AchieveGoalsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.achieve_goals
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AchieveGoalsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AchieveGoalsApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/achieve_goals/AchieveGoalsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.achieve_goals
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AchieveGoalsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
